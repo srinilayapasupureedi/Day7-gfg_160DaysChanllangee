19122be8fa8e3d7cf46abbebaaef9c401b6853ad
srinilayapasupureedi <136550196+srinilayapasupureedi@users.noreply.github.com>
2024-11-30 19:21:19 +0530

    Create code_day7.cpp

---
+++ code_day7.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int cp=prices[0];
+        int maxprofit=0;
+        for(int i=1;i<prices.size();i++)
+        {
+            //curr prices are sellin prices
+            if(prices[i]>cp)
+            {
+                maxprofit+=prices[i]-cp;
+                
+            }
+                
+             cp=prices[i];
+            
+        }
+        return maxprofit;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        int res = ob.maximumProfit(arr);
+        cout << res;
+
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
